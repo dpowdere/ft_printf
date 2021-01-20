0afa697d395a5888aca57ce4f133a7ff5f83422d
Dorine Powdered <dpowdere@student.21-school.ru>
2021-01-20 15:28:04 +0300

    Refactor and normalize

--- ryu/ryu_mul_shift_mod1e9.c
+++ ryu/ryu_mul_shift_mod1e9.c
@@ -14,35 +14,40 @@
 
 #include "ryu.h"
 
-uint32_t	ft_mul_shift_mod1e9(const uint64_t m,
-								const uint64_t *const mul,
-								const int32_t j)
+static inline void	ft_s1(const uint64_t m,
+							const uint64_t *const mul,
+							uint64_t *s1low,
+							uint64_t *s1high)
 {
-	uint64_t high0;																	 // 64
-	const uint64_t low0 = ft_umul128(m, mul[0], &high0); // 0
-	uint64_t high1;																	 // 128
-	const uint64_t low1 = ft_umul128(m, mul[1], &high1); // 64
-	uint64_t high2;																	 // 192
-	const uint64_t low2 = ft_umul128(m, mul[2], &high2); // 128
-	const uint64_t s0low = low0;							// 0
-	(void) s0low; // unused
-	const uint64_t s0high = low1 + high0;		 // 64
-	const uint32_t c1 = s0high < low1;
-	const uint64_t s1low = low2 + high1 + c1; // 128
-	const uint32_t c2 = s1low < low2; // high1 + c1 can't overflow, so compare against low2
-	const uint64_t s1high = high2 + c2;			 // 192
-	// (128 <= j <= 180)
-	if (j < 160)
-	{ // j: [128, 160)
-		const uint64_t r0 = (uint32_t)MOD1E9(s1high);
-		const uint64_t r1 = (uint32_t)MOD1E9((r0 << 32) | (s1low >> 32));
-		const uint64_t r2 = ((r1 << 32) | (s1low & 0xffffffff));
-		return ((uint32_t)MOD1E9(r2 >> (j - 128)));
-	}
-	else
-	{ // j: [160, 192)
-		const uint64_t r0 = (uint32_t)MOD1E9(s1high);
-		const uint64_t r1 = ((r0 << 32) | (s1low >> 32));
-		return ((uint32_t)MOD1E9(r1 >> (j - 160)));
-	}
+	uint64_t hi0;
+	uint64_t hi1;
+	uint64_t hi2;
+	uint64_t lo1;
+	uint64_t lo2;
+
+	(void)ft_umul128(m, mul[0], &hi0);
+	lo1 = ft_umul128(m, mul[1], &hi1);
+	lo2 = ft_umul128(m, mul[2], &hi2);
+	*s1low = lo2 + hi1 + (lo1 + hi0 < lo1);
+	*s1high = hi2 + (*s1low < lo2);
 }
+
+uint32_t			ft_mul_shift_mod1e9(const uint64_t m,
+										const uint64_t *const mul,
+										const int32_t j)
+{
+	uint64_t s1low;
+	uint64_t s1high;
+
+	ft_s1(m, mul, &s1low, &s1high);
+	if (j < 160)
+		return ((uint32_t)MOD1E9((uint64_t)((((uint64_t)(uint32_t)MOD1E9(
+			(((uint64_t)(uint32_t)MOD1E9(s1high)) << 32) | (s1low >> 32)))
+			<< 32) | (s1low & 0xffffffff)) >> (j - 128)));
+	else
+		return ((uint32_t)MOD1E9(((((uint64_t)(uint32_t)MOD1E9(s1high)) << 32)
+			| (s1low >> 32)) >> (j - 160)));
+}
+/*
+** (128 <= j <= 180)
+*/
