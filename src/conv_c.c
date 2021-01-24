a54e16b4d14de7fce5407e30262455fbd54028b7
Dorine Powdered <dpowdere@student.21-school.ru>
2021-01-24 18:23:39 +0300

    Use wctomb instead of wcstombs for %lc

--- src/conv_c.c
+++ src/conv_c.c
@@ -19,18 +19,20 @@
 #define UTF8_MAX_BYTES_PER_CHAR		6
 #define SINGLE_UTF8_CHAR_STRING		(UTF8_MAX_BYTES_PER_CHAR + 1)
 
+/*
+** See also stdlib's MB_CUR_MAX, wctomb, wcstombs.
+*/
+
 void	ft_conv_c(t_toolbox *toolbox, va_list *arg_ptr)
 {
 	int		c;
+	int		len;
 	char	s[SINGLE_UTF8_CHAR_STRING];
-	size_t	len;
-	wchar_t	wc;
 
 	if (toolbox->spec.size == SIZE_L)
 	{
 		ft_memset(s, 0, SINGLE_UTF8_CHAR_STRING);
-		wc = va_arg(*arg_ptr, wchar_t);
-		if ((len = wcstombs(s, &wc, SINGLE_UTF8_CHAR_STRING)) == (size_t)-1)
+		if ((len = wctomb(s, va_arg(*arg_ptr, wchar_t))) == -1)
 			toolbox->error = PRINTF_WCHAR_CONVERT_ERROR;
 		ft_print_field((char *)s, len, toolbox);
 	}
